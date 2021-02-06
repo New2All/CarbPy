7827b0874a552e379db39e44d4aaa4f227df5b32
Michael Prior <75118205+New2All@users.noreply.github.com>
2021-02-06 17:26:59 +0100

    Update carbpy.py

--- carbpy.py
+++ carbpy.py
@@ -4,7 +4,7 @@
  #        From/to Locations are identified and resolved to coordinates, then distance in kilometers calculated using geopy utilities.
  #        GHG emissions (gCO2e/km) per transport mode are derived from 'our world in data' table extract.
  #        To note that this version serves as a prototype or proof-of-concept. It is not meant as a finished product. 
- #        Run environment dependencies include pre-installation of geopy and PySimpleGUI modules. See Pypy for details.
+ #        Run environment dependencies include pre-installation of geopy and PySimpleGUI modules. See Pypi for details.
  #        Version 0.1 Beta .
  #=================================================================================================================================*
  #
